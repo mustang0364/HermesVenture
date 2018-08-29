8bf6b62125419a6c54bfbf7c02fc832589834ac7
speeas <speeas@gmail.com>
2018-08-29 16:37:01 -0700

    fixed server latency get errors

--- hermes_venture/src/App.js
+++ hermes_venture/src/App.js
@@ -4,6 +4,7 @@ import Routes from './Routes';
 import Dashboard from './components/Dashboard.js';
 import Home from './components/Home.js';
 import axios from 'axios';
+import DashboardArrow from './Media/Images/dashboard-arrow.png';
 
 class App extends Component {
   constructor(){
@@ -15,9 +16,11 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    axios.get('/dashboard').then(res => {
-        this.setState({products: res.data})
-    })
+    setTimeout(() => {
+      axios.get('/dashboard').then(res => {
+          this.setState({products: res.data})
+      })   
+    }, 500);
 }
   redirect = () => {
     this.setState({
@@ -32,6 +35,9 @@ class App extends Component {
       ? <Routes /> 
       : <div><Home products={this.state.products} redirect={this.redirect} /> <Dashboard needsRedirect={this.state.redirect} products={this.state.products} redirect={this.redirect} /> <Routes /></div>
       }
+        <div className="footer">        
+              <a href="#dashboard"><img src={DashboardArrow} alt=""/></a>
+        </div>
       </div>
     );
   }
