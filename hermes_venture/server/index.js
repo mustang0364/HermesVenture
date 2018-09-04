729e5c943a8a403625bf5a5454e19e6a342f65c8
abraham martinez <abrahammartinez@abrahams-MacBook-Pro.local>
2018-09-04 12:02:07 -0700

    auth0 hhtps

--- hermes_venture/server/index.js
+++ hermes_venture/server/index.js
@@ -79,7 +79,7 @@ app.get('/auth/callback', (req, res) => {
       client_secret: process.env.AUTH0_CLIENT_SECRET,
       code: req.query.code,
       grant_type: 'authorization_code',
-      redirect_uri: `http://${req.headers.host}/auth/callback`
+      redirect_uri: `https://${req.headers.host}/auth/callback`
   }
 
   function tradeCodeForAccessToken() {
